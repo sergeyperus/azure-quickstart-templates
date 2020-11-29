7563112cd4df4138fc9bbfbec596fee67f067200
Sergey Pechenkin <45074930+sergeyperus@users.noreply.github.com>
2020-11-29 00:01:30 +0000

    1

--- 010-ARM-Lesson-1/deployit.ps1
+++ 010-ARM-Lesson-1/deployit.ps1
@@ -1,4 +1,8 @@
+
 Enable-AzureRMAlias #Включить режим совместимости алиасов с Rm
+
+Connect-AzAccount
+Save-AzContext -Path $env:OneDriveCommercial\Технологии\Azure\_Profiles\msprofile.json
 Import-AzContext -Path $env:OneDriveCommercial\Технологии\Azure\_Profiles\msprofile.json
 
 $templateURI = "https://raw.githubusercontent.com/sergeyperus/azure-quickstart-templates/master/010-ARM-Lesson-1/azuredeploy.json"
@@ -6,3 +10,9 @@ New-AzResourceGroupDeployment  `
     -Name addstorage `
     -ResourceGroupName "ARM-Lessons"  `
     -TemplateURI $templateURI
+
+    get-help Connect-AzAccount -Examples
+    
+    Connect-AzAccount -ServicePrincipal -ApplicationId f53bdcbb-3407-48c5-8950-f21ab84242db -Tenant 72f988bf-86f1-41af-91ab-2d7cd011db47
+    
+    Connect-AzAccount -Credential $Credential -Tenant 72f988bf-86f1-41af-91ab-2d7cd011db47 -ServicePrincipal
